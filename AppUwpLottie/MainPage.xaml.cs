d2bc3c3cf5f14aefa5fad68004e43999adc347fe
jjaehrig <jens-uwe.jaehrig@fsd-web.de>
2021-02-19 18:53:55 +0100

    more examples

--- AppUwpLottie/MainPage.xaml.cs
+++ AppUwpLottie/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,29 @@ namespace AppUwpLottie
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        readonly Dictionary<string, Uri> examples = new Dictionary<string, Uri>()
+        {
+            ["6596-countdown-2"] = new Uri("https://assets5.lottiefiles.com/packages/lf20_SkQZHu.json"),
+            ["6595-countdown-1"] = new Uri("https://assets5.lottiefiles.com/packages/lf20_8YWSr5.json"),
+            ["45-countdown"] = new Uri("https://assets5.lottiefiles.com/datafiles/pNpCU01WQ7qDWgK/data.json"),
+            ["44014-countdown"] = new Uri("https://assets5.lottiefiles.com/packages/lf20_jzggnssm.json"),
+            ["42000-countdown"] = new Uri("https://assets5.lottiefiles.com/packages/lf20_689pdhcy.json"),
+            ["41999-countdown"] = new Uri("https://assets5.lottiefiles.com/packages/lf20_j7totias.json"),
+            ["35589-countdown"] = new Uri("https://assets5.lottiefiles.com/packages/lf20_ptc6mday.json"),
+        };
+
         public MainPage()
         {
             this.InitializeComponent();
+            ApplicationView.PreferredLaunchViewSize = new Size { Width = 800, Height = 400 };
+
+            foreach (var key in examples.Keys) furtherExamples.Items.Add(key);
+            furtherExamples.SelectionChanged += (_, a) =>
+            {
+                string animationId = furtherExamples.SelectedItem as string;
+                lottieFurther.UriSource = examples[animationId];
+                hlGotoLottieFiles.NavigateUri = new Uri($"https://lottiefiles.com/{animationId}");
+            };
         }
 
         private void tbUri_KeyDown(object sender, KeyRoutedEventArgs e)
